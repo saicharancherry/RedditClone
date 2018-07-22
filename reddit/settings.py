80dbe8b97c5c6e89701dadd434027efa41dcebac
saicharan peda <saicharancherry593@gmail.com>
2018-07-22 16:20:26 +0530

    Update settings.py

--- reddit/settings.py
+++ reddit/settings.py
@@ -14,7 +14,8 @@ import os
 import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR =os.path.dirname(os.path.abspath(__file__)) #os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
