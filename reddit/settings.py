e458b22caad606f5a92bc766a4e780bf23d4b540
saicharancherry <saicharancherry593@gmail.com>
2018-07-23 04:53:28 +0530

    commit

--- reddit/settings.py
+++ reddit/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import decouple
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR =os.path.dirname(os.path.abspath(__file__)) #os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -132,7 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'build/static'),
+  os.path.join(BASE_DIR, '/reddit-frontend'),
+os.path.join(BASE_DIR, '/reddit-frontend/src'),
+os.path.join(BASE_DIR, '/reddit-frontend/public'),
 ]
 
 WEBPACK_LOADER = {
