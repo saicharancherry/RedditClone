1dce1dff9f43f37ed0aaa7c67f5046a505930b5e
saicharancherry <saicharancherry593@gmail.com>
2018-07-25 06:53:15 +0530

    'c'

--- reddit/settings.py
+++ reddit/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import datetime
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+db_from_env=dj_database_url.config()
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -91,7 +94,7 @@ DATABASES = {
     }
 }
 
-
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
