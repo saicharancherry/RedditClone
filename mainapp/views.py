819a26202f8382f2f639c551ca05da32457af082
saicharancherry <saicharancherry593@gmail.com>
2018-07-25 06:59:10 +0530

    'c'

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,7 +10,6 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-import ipdb
 from django.utils import timezone
 import django
 from rest_framework.generics import CreateAPIView
